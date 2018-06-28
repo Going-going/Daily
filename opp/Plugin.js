9ee66e1a6ccfbdf6bdb54d152c9aa74987e41f29
赵扬悦 <13706342556@163.com>
2018-06-28 23:35:41 +0800

    修改iai

--- opp/Plugin.js
+++ opp/Plugin.js
@@ -32,18 +32,10 @@
             activeIndex: 0,
             autoPlay: false,
             plagination: false,
+            navigation: true,
             callback: function() {}
         }, options)
 
-        // if(options.url === '' || options.url === undefined || options.url === null && data){
-        //     console.log('url不存在')
-        // } 
-        // if(options.data === '' || options.data === undefined || options.data === null && data){
-        //     console.log('data不存在')
-        // } 
-        // if(options.url && options.data){
-            
-        // }
         if(options.url){
             var xmlHttp;
             if(window.XMLHttpRequest){
@@ -53,14 +45,12 @@
             }
             xmlHttp.open('get', options.url, false);
             xmlHttp.send();
-            // xmlHttp.onreadystatechange = function(){
-                if(xmlHttp.status === 200 && xmlHttp.readyState === 4){
-                    console.log(JSON.parse(xmlHttp.responseText))
-                    options.data = JSON.parse(xmlHttp.responseText);
-                }
-            // }
+            if(xmlHttp.status === 200 && xmlHttp.readyState === 4){
+                console.log(JSON.parse(xmlHttp.responseText))
+                options.data = JSON.parse(xmlHttp.responseText);
+            }
         }
-        var plagination = [], slider = [];
+        var plagination = [], slider = [], $this = $(this);
         for(var i = 0, length = options.data.length; i < length; i++){
             slider.push('<li class="slide"><img src="'+ options.data[i].img +'" alt=""></li>');
             if(options.plagination){
@@ -73,21 +63,87 @@
         }
         this.children('.wrapper').html(slider.join(' '));
         this.children('.focusBox').html(plagination.join(' '))
-        // var width = this.children('.wrapper').find('li').width() * options.data.length;
-        // this.children('.wrapper').width(width)
-        var _this = this;
+
+        changePosition();
+        goSlide();
+
         if(options.autoPlay){
-            var time = options.autoPlay || 2000;
+            var time = typeof options.autoPlay === 'number' ? options.autoPlay : 2000;
+
             var i = 1;
-            var timer = setInterval(function() {
-                _this.children('.wrapper').find('li').eq(i).css('left', '0').animate({
-                    left: '-100%'
-                })
-                i++;
-                if(i > options.data.length){
-                    i = 0;
-                }
-            }, time)
+            var timer = setInterval(autoPlay, time)
         }
+
+        if(options.navigation){
+            navigation()
+            // $this.children('.wrapper').append('<a href="javascript:;" class="arrow arrowLeft"></a><a href="javascript:;" class="arrow arrowRight"></a>')
+        }
+        function autoPlay(){
+            var p = $this.children('.wrapper').find('.slide').eq(0).width() * i;
+            animateSlide(p)
+            matchPla();
+            i++;
+            if(i > options.data.length - 1){
+                i = 0;
+            }
+        }
+        function navigation() {
+            var scrollLeft = parseFloat($this.children('.wrapper').css('left'));
+            alert(scrollLeft <= 0)
+            if(scrollLeft <= 0){
+                $this.children('.wrapper').append('<a href="javascript:;" class="arrow arrowRight"></a>')
+            }else{
+                $this.children('.wrapper').append('<a href="javascript:;" class="arrow arrowLeft"></a><a href="javascript:;" class="arrow arrowRight"></a>')
+            }
+        }
+        function animateSlide(p){
+            $this.children('.wrapper').stop().animate({
+                left: '-' + p + 'px'
+            });
+        }
+
+        function addClass(currIndex) {
+            $this.children('.wrapper').find('.slide').eq(currIndex).addClass('active').siblings().removeClass('active');
+            $this.children('.focusBox').children().eq(currIndex).addClass('select').siblings().removeClass('select');
+        }
+
+        // slide样式
+        function changePosition() {
+            $this.children('.wrapper').find('.slide').each(function(index, item) {
+                var l = $this.children('.wrapper').find('.slide').eq(0).width() * index;
+                item.style.left = l + 'px';
+            })
+        }
+
+        // 改变底部小圆点位置
+        function matchPla() {
+            $this.children('.wrapper').find('.slide').each(function(index, item) {
+                var scrollLeft = parseFloat($this.children('.wrapper').css('left'));
+                var currLeft = parseFloat(item.style.left);
+                if(currLeft == Math.abs(scrollLeft)){
+
+                    var curindex = $(item).index();
+                    if(curindex >= options.data.length - 1){
+                        curindex = -1;
+                    }
+                    addClass(curindex+1)
+                }
+            })
+        }
+        // 点击小圆点跳转到响应slide
+        function goSlide() {
+            $this.hover(function() {
+                if(timer) clearInterval(timer);
+            }, function () {
+                timer = setInterval(autoPlay, time)
+            })
+            $this.children('.focusBox').children().on('click', function () {
+                i = $(this).index();
+                addClass($(this).index())
+                var p = $this.children('.wrapper').find('.slide').eq(0).width() * $(this).index()
+                animateSlide(p)
+            })
+        }
+
     }
 })(jQuery)
