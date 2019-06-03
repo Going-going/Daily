896befd1d42b2e1ce36203eba1065314642c070c
Demon <15315512115@163.com>
2019-06-03 10:07:51 +0800

    提交

---
+++ vuetodolist/src/pages/login.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div>
+            用户名<input type="text" v-model="username" />
+        </div>
+        <div>
+            密码<input type="text" v-model="password" />
+        </div>
+        <button @click="loginIn">登录</button>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        loginIn() {
+            window.localStorage.setItem('username', this.username);
+            window.localStorage.setItem('password', this.password);
+            this.$route.push('/index')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- vuetodolist/src/router/index.js
+++ vuetodolist/src/router/index.js
@@ -5,37 +5,43 @@ import List from '../pages/list.vue'
 import Favor from '../pages/favor.vue'
 import Add from '../pages/add.vue'
 import Change from '../pages/change.vue'
+import Login from '../pages/login.vue'
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'index',
-      component: Index
-    }, {
-      path: '/list',
-      name: 'list',
-      component: List,
-      meta: {
-        keepAlive: true
-      }
-    }, {
-      path: '/favor',
-      name: 'favor',
-      component: Favor,
-      meta: {
-        keepAlive: true
-      }
-    }, {
-      path: '/add',
-      name: 'add',
-      component: Add
-    }, {
-      path: '/change',
-      name: 'change',
-      component: Change
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/index',
+            name: 'index',
+            component: Index
+        }, {
+            path: '/list',
+            name: 'list',
+            component: List,
+            meta: {
+                keepAlive: true
+            }
+        }, {
+            path: '/favor',
+            name: 'favor',
+            component: Favor,
+            meta: {
+                keepAlive: true
+            }
+        }, {
+            path: '/add',
+            name: 'add',
+            component: Add
+        }, {
+            path: '/change',
+            name: 'change',
+            component: Change
+        }
+    ]
 })
